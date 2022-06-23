5bbc34428c5915aa7bf1d73bf31e7a88c1fd8e7d
Anuj Punjani <some4gun@gmail.com>
2022-06-23 23:45:50 +0530

    Time: 43 ms (59.67%), Space: 117.8 MB (42.03%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    static class Pair implements Comparable<Pair>{
+        int x, y;
+        double distancefromorigin;
+        
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+            distancefromorigin = Math.sqrt(x*x + y*y);
+        }
+        
+        @Override
+        public int compareTo(Pair pair) {
+            if(distancefromorigin < pair.distancefromorigin)
+                return -1;
+            return 1;
+        }
+    }
+    
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        
+        for(int[] point : points) {
+            pq.add(new Pair(point[0], point[1]));
+        }
+        
+        int[][] ans = new int[k][2];
+        
+        int idx = 0;
+        while(k --> 0) {
+            Pair temp = pq.remove();
+            ans[idx][0] = temp.x;
+            ans[idx][1] = temp.y;
+            idx++;
+        }
+        
+        return ans;
+    }
+}
