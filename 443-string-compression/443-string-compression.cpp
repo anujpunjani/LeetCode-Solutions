5babe33e431abfec2c4dafb5e6e934f6f1c7be49
Anuj Punjani <some4gun@gmail.com>
2022-06-28 17:40:19 +0530

    Time: 4 ms (84.88%), Space: 9.1 MB (24.98%) - LeetHub

---
+++ 443-string-compression/443-string-compression.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        
+        int count = 1;
+        char last = chars[0];
+        int index = 0;
+        for(int i = 1; i < chars.size(); i++) {
+            
+            if(last != chars[i]) {
+                if(count != 1) {
+                    
+                    chars[index++] = last;
+
+                    if(count > 9) {
+                        string temp = to_string(count);
+                        for(char ch : temp)
+                            chars[index++] = ch;
+                    }else
+                        chars[index++] = (count + '0');
+                } else {
+                    chars[index++] = last;
+                }
+                
+                last = chars[i];
+                count = 1;
+                
+            } else {
+                count++;
+            }
+            
+        }
+        if(count != 1) {
+            chars[index++] = last;
+            
+            if(count > 9) {
+                string temp = to_string(count);
+                for(char ch : temp)
+                    chars[index++] = ch;
+            }else
+                chars[index++] = (count + '0');
+            
+        } else {
+            chars[index++] = last;
+        }
+        return index;
+    }
+};
