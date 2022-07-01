294ca63ebbb98ae9b6f38a4ea160cc67672e3445
Anuj Punjani <some4gun@gmail.com>
2022-07-01 13:24:25 +0530

    Time: 10 ms (79.50%), Space: 51.2 MB (33.19%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -1,8 +1,17 @@
 class Solution {
     
+    class Compare implements Comparator<int[]> {
+        
+        public int compare(int[] a, int[] b){
+            return b[1] - a[1];            
+        }
+        
+    }
+    
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         
-        Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+        // Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+        Arrays.sort(boxTypes, new Compare());
         
         int ans = 0;
         
