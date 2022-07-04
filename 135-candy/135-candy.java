5b1f55d39c5fad217b787c7b2ad1916f0c3257f5
Anuj Punjani <some4gun@gmail.com>
2022-07-04 23:48:22 +0530

    Time: 6 ms (29.60%), Space: 51.9 MB (38.56%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -2,23 +2,26 @@ class Solution {
     public int candy(int[] ratings) {
         
         int ans = 0;
-        int[] l2r = new int[ratings.length];
-        int[] r2l = new int[ratings.length];
+        int[] candies = new int[ratings.length];
         
-        Arrays.fill(l2r, 1);
-        Arrays.fill(r2l, 1);
+        Arrays.fill(candies, 1);
         
-        for(int i = 1, j = ratings.length - 2; i < ratings.length; i++, j--) {
+        for(int i = 1; i < ratings.length; i++) {
             
             if(ratings[i] > ratings[i-1])
-                l2r[i] = l2r[i-1]+1;
+                candies[i] = candies[i-1]+1;
             
-            if(ratings[j] > ratings[j+1])
-                r2l[j] = r2l[j+1]+1;
         }
         
-        for(int i = 0; i < ratings.length; i++)
-            ans += Math.max(l2r[i], r2l[i]);
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            
+            if(ratings[i] > ratings[i+1])
+                candies[i] = Math.max(candies[i], candies[i+1]+1);
+            
+        }
+        
+        for(int candy : candies)
+            ans += candy;
         
         return ans;
     }
