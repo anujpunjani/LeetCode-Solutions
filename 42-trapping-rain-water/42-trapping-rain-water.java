8400e5a9ca42888ab4f0d3bc8d7b1f199316be9b
Anuj Punjani <some4gun@gmail.com>
2022-07-07 18:12:25 +0530

    Time: 1 ms (99.37%), Space: 42.9 MB (91.31%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,24 +1,27 @@
 class Solution {
     public int trap(int[] height) {
         
-        int n = height.length;
-        int prefixSum[] = new int[n];
-        int suffixSum[] = new int[n];
+        int n = height.length, ans = 0;
+        int left = 0, leftMax = 0;
+        int right = n - 1, rightMax = 0;
         
-        prefixSum[0] = height[0];
-        suffixSum[n - 1] = height[n - 1];
-        
-        for(int i = 1, j = n - 2; i < n; i++, j--) {
+        while(left <= right) {
             
-            prefixSum[i] = Math.max(prefixSum[i - 1], height[i]);
-            suffixSum[j] = Math.max(suffixSum[j + 1], height[j]);
+            if(height[left] <= height[right]) {
+                if(leftMax < height[left])
+                    leftMax = height[left];
+                else
+                    ans += leftMax - height[left];
+                left++;
+            } else {
+                if(rightMax < height[right])
+                    rightMax = height[right];
+                else
+                    ans += rightMax - height[right];
+                right--;
+            }
             
         }
-
-        int ans = 0;
-        
-        for(int i = 0; i < n; i++)
-            ans += Math.min(prefixSum[i], suffixSum[i]) - height[i];
         
         return ans;
     }
