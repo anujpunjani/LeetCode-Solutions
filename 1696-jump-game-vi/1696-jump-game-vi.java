4e1ff482a9bd60e2fd1179fb78c2989908b3349b
Anuj Punjani <some4gun@gmail.com>
2022-07-09 19:31:41 +0530

    Time: 99 ms (26.81%), Space: 114.4 MB (31.71%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0]-a[0]);
+        
+        int[] dp = new int[n];
+        dp[n-1] = nums[n-1];
+        pq.add(new int[]{nums[n-1], n-1});
+        for(int i = n-2; i >= 0; i--) {
+            
+            while(pq.size() > 0 && pq.peek()[1] > i + k)
+                pq.remove();
+            
+            dp[i] = nums[i] + pq.peek()[0];
+            pq.add(new int[]{dp[i], i});
+        }
+        
+        return dp[0];
+    }
+}
