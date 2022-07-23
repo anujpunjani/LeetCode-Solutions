a21f7599f015003b7fe5becb7d1e10d501772aa5
Anuj Punjani <some4gun@gmail.com>
2022-07-23 14:27:50 +0530

    Time: 178 ms (52.57%), Space: 45.6 MB (17.97%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    class Pair {
+        public:
+            int node;
+            int weight;
+
+            Pair() {}
+        
+            Pair(int node, int wt) {
+                this->node = node;
+                this->weight = wt;
+            }
+
+            bool operator()(Pair a, Pair b) {
+                return a.weight > b.weight;
+            }
+    };
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        
+        n++;
+        vector<vector<Pair>> graph(n);
+        for(auto time : times) 
+            graph[time[0]].emplace_back(time[1], time[2]);
+        
+        
+        vector<int> vis(n, -1);
+        vis[0] = 0;
+        priority_queue<Pair, vector<Pair>, Pair> pq;
+        pq.push(Pair(k, 0));
+        
+        while(pq.size() > 0) {
+            Pair top = pq.top();
+            pq.pop();
+            
+            if(vis[top.node] != -1) continue;
+            vis[top.node] = top.weight;
+            
+            for(Pair nbr : graph[top.node])
+                pq.push(Pair(nbr.node, nbr.weight + top.weight));
+        }
+        int ans = 0;
+        for(int val : vis) {
+            if(val == -1) return -1;
+            ans = max(ans, val);
+        }
+        return ans;
+    }
+};
