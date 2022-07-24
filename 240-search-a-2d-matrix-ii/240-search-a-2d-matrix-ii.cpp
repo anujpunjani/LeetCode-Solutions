b3b7034f69843c9301c47df239e0edb93cd8fff6
Anuj Punjani <some4gun@gmail.com>
2022-07-24 13:45:43 +0530

    Time: 242 ms (26.23%), Space: 14.9 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -4,41 +4,15 @@ public:
         
         int n = matrix.size() - 1, m = matrix[0].size() - 1;
         int row = 0, col = m;
-        
-        if(matrix[row][col] == target)
-            return true;
-        
-        while(col > 0 && row < n) {
-            // cout << matrix[row][col];
-            if(matrix[row][col - 1] == target || matrix[row + 1][col] == target)
+
+        while(col >= 0 && row <= n) {
+            if(matrix[row][col] == target)
                 return true;
             
-            if(matrix[row][col - 1] < target && matrix[row + 1][col] < target) {
-                if(matrix[row][col - 1] > matrix[row + 1][col])
-                    col = col - 1;
-                else
-                    row = row + 1;
-                
-            } else if (matrix[row][col - 1] > target && matrix[row + 1][col] > target) {
-                if(matrix[row][col - 1] < matrix[row + 1][col])
-                    col = col - 1;
-                else
-                    row = row + 1;
-            } else if(matrix[row][col - 1] > target && matrix[row + 1][col] < target) {
-                row = row + 1;
-            } else if(matrix[row][col - 1] < target && matrix[row + 1][col] > target) {
-                col = col - 1;
-            }
-        }
-        while(row <= n) {
-            if(matrix[row][0] == target)
-                return true;
-            row++;
-        }
-        while(col >= 0) {
-            if(matrix[n][col] == target)
-                return true;
-            col--;
+            if(matrix[row][col] < target)
+                row++;
+            else
+                col--;
         }
         return false;
     }
