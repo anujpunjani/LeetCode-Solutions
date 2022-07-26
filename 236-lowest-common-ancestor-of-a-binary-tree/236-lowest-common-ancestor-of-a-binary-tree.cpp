23c272ef134ec7526dc20e9cbf2faa11e69653c5
Anuj Punjani <some4gun@gmail.com>
2022-07-26 08:35:14 +0530

    Time: 16 ms (89.68%), Space: 14.3 MB (25.30%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,34 +9,20 @@
  */
 class Solution {
 public:
-    
-    vector<TreeNode*> n2r(TreeNode* node, TreeNode* p) {
-        if(node == nullptr) return vector<TreeNode*>();
-        
-        if(node == p) return vector<TreeNode*> {node};
-        
-        vector<TreeNode*> left = n2r(node->left, p);
-        if(left.size() > 0) {
-            left.push_back(node);
-            return left;
-        }
-        vector<TreeNode*> right = n2r(node->right, p);
-        if(right.size() > 0) {
-            right.push_back(node);
-            return right;
-        }
-        
-        return vector<TreeNode*>();
-    }
-    
+
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> ppath = n2r(root, p);
-        vector<TreeNode*> qpath = n2r(root, q);
-        reverse(ppath.begin(), ppath.end());
-        reverse(qpath.begin(), qpath.end());
-        int i = 0, j = 0;
-        while(i < ppath.size() && j < qpath.size() && ppath[i] == qpath[j])
-            i++, j++;
-        return ppath[i-1];
+        if(root == nullptr) return root;
+        
+        if(root == p || root == q)
+            return root;
+        
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if(left != nullptr && right != nullptr) return root;
+        
+        if(left != nullptr) return left;
+        
+        return right;
     }
 };
