3897f40b9463cb3a035af4c005e86fa9633ff824
Anuj Punjani <some4gun@gmail.com>
2022-08-11 21:39:22 +0530

    Time: 182 ms (17.94%), Space: 128.3 MB (12.60%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -1,66 +1,51 @@
 class Solution {
-    
-    class Pair implements Comparable<Pair>{
-        int stop;
-        int color;
-        int changes;
-        
-        Pair(int stop, int color, int changes) {
-            this.stop = stop;
-            this.color = color;
-            this.changes = changes;
-        }
-        
-        public int compareTo(Pair other) {
-            return this.changes - other.changes;
-        }
-    }
-    
     public int numBusesToDestination(int[][] routes, int source, int target) {
         
         if(source == target) return 0;
-        int n = 0;
         
-        for(int[] route : routes)
-            for(int stop : route)
-                n = Math.max(stop, n);
+        HashMap<Integer, HashSet<Integer>> graph = new HashMap<>();
+        HashSet<Integer> busesVisited = new HashSet();
+        HashSet<Integer> StopsVisited = new HashSet();
         
-        ArrayList<Pair>[] graph = new ArrayList[n + 1];
-        for(int i = 0; i < n + 1; i++)
-            graph[i] = new ArrayList<>();
-        
-        for(int i = 0; i < routes.length; i++)
+        for(int i = 0; i < routes.length; i++) {
             for(int j = 0; j < routes[i].length; j++) {
-                int src = routes[i][j];
-                int dest = routes[i][(j + 1) % routes[i].length];
                 
-                graph[src].add(new Pair(dest, i, 0));
-                graph[dest].add(new Pair(src, i, 0));
+                if(!graph.containsKey(routes[i][j]))
+                    graph.put(routes[i][j], new HashSet<>());
+                
+                graph.get(routes[i][j]).add(i);
             }
+        }
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        pq.add(new Pair(source, routes.length, 0));
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        StopsVisited.add(source);
+        int changes = 0;
         
-        int[][] vis = new int[n + 1][routes.length + 1];
-        for(int i = 0; i <= n; i++)
-            for(int j = 0; j <= routes.length; j++)
-                vis[i][j] = -1;
-        
-        while(pq.size() > 0) {
+        while(q.size() > 0) {
             
-            Pair top = pq.remove();
-            if(top.stop == target) return top.changes;
-            
-            if(vis[top.stop][top.color] != -1) continue;
-            vis[top.stop][top.color] = top.changes;
-            
-            for(Pair nbr : graph[top.stop]) {
-                if(nbr.color == top.color)
-                    pq.add(new Pair(nbr.stop, nbr.color, top.changes));
-                else
-                    pq.add(new Pair(nbr.stop, nbr.color, top.changes + 1));
+            int size = q.size();
+            while(size --> 0) {
+                int front = q.remove();
+                if(front == target) return changes;
+                
+                HashSet<Integer> buses = graph.get(front);
+                for(int bus : buses) {
+                    
+                    if(busesVisited.contains(bus))
+                        continue;
+                    
+                    int[] stops = routes[bus];
+                    for(int stop : stops) {
+                        if(StopsVisited.contains(stop))
+                            continue;
+                        q.add(stop);
+                        StopsVisited.add(stop);
+                    }
+                    busesVisited.add(bus);
+                }
             }
-            
+            changes++;
         }
         
         return -1;
