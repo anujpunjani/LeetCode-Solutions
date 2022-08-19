c523070c2baa18e71ec093f0fada2e24a368648c
Anuj Punjani <some4gun@gmail.com>
2022-08-19 17:30:25 +0530

    Time: 17 ms (55.00%), Space: 41.6 MB (52.38%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,15 +1,19 @@
 class Solution {
     
     private boolean check(char[][] board, char n, int x, int y) {
-
-        for(int i = 0; i < 9; i++) {
-            
-            if(board[x][i] == n) return false;
-
-            if(board[i][y] == n) return false;
-
-            if(board[3 *(x/3) + i/3][3 * (y/3) + i%3] == n) return false;
-        }
+        
+        for(int i = 0; i < board.length; i++)
+             if(board[x][i] == n) return false;
+        
+        for(int i = 0; i < board.length; i++)
+             if(board[i][y] == n) return false;
+        
+        x = x%3 == 0? x : x - x%3;
+        y = y%3 == 0? y : y - y%3;
+        
+        for(int i = x; i < x + 3; i++)
+            for(int j = y; j < y + 3; j++)
+                if(board[i][j] == n) return false;
         
         return true;
     } 
@@ -17,7 +21,7 @@ class Solution {
     
     public boolean solve(char[][] board) {
         for(int i = 0; i < board.length; i++)
-            for(int j = 0; j < board[0].length; j++) 
+            for(int j = 0; j < board.length; j++) {
                 if(board[i][j] == '.') {
                     for(char k = '1'; k <= '9'; k++)
                         if(check(board, k, i, j)) {
@@ -28,7 +32,8 @@ class Solution {
                         }    
                     return false;
                 }
-
+            }
+        
         return true;
     }
     
