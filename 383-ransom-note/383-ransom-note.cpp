eb81db82e9bffcce3c0468267345dee5e4d3de99
Anuj Punjani <some4gun@gmail.com>
2022-08-25 12:22:35 +0530

    Time: 26 ms (52.04%), Space: 8.7 MB (72.69%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> m;
+        for(char ch : magazine)
+            m[ch]++;
+        
+        for(char ch : ransomNote) {
+            m[ch]--;
+            if(m[ch] < 0) return false;
+        }
+        
+        return true;
+    }
+};
