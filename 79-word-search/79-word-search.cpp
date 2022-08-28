30409002da38ba2ec274861d0d3a924d23ceeaf4
Anuj Punjani <some4gun@gmail.com>
2022-08-28 20:27:11 +0530

    Time: 1185 ms (12.44%), Space: 8 MB (77.94%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     
-    bool exist(int r, int c, int idx, vector<vector<char>> &board, string word) {
+    bool ans = false;
+    
+    void exist(int r, int c, int idx, vector<vector<char>> &board, string word) {
         
-        if(idx == word.size()) return true;
+        if(idx == word.size()) {
+            ans = true;
+            return;
+        }
         
-        if(r < 0 || c < 0 || r == board.size() || c == board[0].size() || board[r][c] != word[idx] || board[r][c] == '0') return false;   
+        if(r < 0 || c < 0 || r == board.size() || c == board[0].size() || board[r][c] != word[idx] || board[r][c] == '0') return;
+        
+        if(ans == true) return;
         
         char temp = board[r][c];
         board[r][c] = '0';
-        
-        bool right = exist(r + 1, c, idx + 1, board, word);
-        bool left = exist(r - 1, c, idx + 1, board, word);
-        bool bottom = exist(r, c + 1, idx + 1, board, word);
-        bool top = exist(r, c - 1, idx + 1, board, word);
-        
+        exist(r + 1, c, idx + 1, board, word);
+        exist(r - 1, c, idx + 1, board, word);
+        exist(r, c + 1, idx + 1, board, word);
+        exist(r, c - 1, idx + 1, board, word);
         board[r][c] = temp;
-        
-        return right || left || top || bottom;
     }
     
     bool exist(vector<vector<char>>& board, string word) {
         for(int i = 0; i < board.size(); i++)
             for(int j = 0; j < board[0].size(); j++)
-                if(exist(i, j, 0, board, word) == true) return true;
+                if(board[i][j] == word[0] && ans == false)
+                    exist(i, j, 0, board, word);
 
-        return false;
+        return ans;
     }
 };
