99f5a87bd56bca7efe7c74ed54bf3ab1ec38122a
Anuj Punjani <some4gun@gmail.com>
2022-08-30 09:53:17 +0530

    Time: 4 ms (21.32%), Space: 6 MB (45.08%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int n) {    
+        string ans = "";
+    
+        while(n > 0) {
+            n--;
+            ans += char(n%26 + 'A');
+            n /= 26;
+        }
+        
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
