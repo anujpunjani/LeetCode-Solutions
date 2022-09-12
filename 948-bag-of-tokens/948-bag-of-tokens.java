1c33f0c8271663d238be79ef8ca9369714c8213f
Anuj Punjani <some4gun@gmail.com>
2022-09-12 10:25:31 +0530

    Time: 2 ms (100.00%), Space: 41.7 MB (97.19%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.java
+++ 948-bag-of-tokens/948-bag-of-tokens.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(tokens);
         
         int i = 0, j = tokens.length - 1;
-        int maxScore = 0, score = 0;
+        int score = 0;
         
-        while(i <= j) {
+        while(i < j) {
             
             if(power < tokens[i] && score == 0) return score;
             
@@ -18,10 +18,11 @@ class Solution {
                 score++;
                 i++;
             }
-            
-            maxScore = Math.max(maxScore, score);
+
         }
         
-        return maxScore;
+        if(i == j && power >= tokens[i]) score++;
+        
+        return score;
     }
 }
