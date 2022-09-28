71769a08e490e33c557ed19a0d3aab2c3cfa3e77
Anuj Punjani <some4gun@gmail.com>
2022-09-28 14:03:09 +0530

    Time: 45 ms (26.06%), Space: 53.9 MB (74.52%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.java
+++ 838-push-dominoes/838-push-dominoes.java
@@ -1,40 +1,37 @@
 class Solution {
     public String pushDominoes(String dominoes) {
+        int n = dominoes.length();
+        char[] ans = new char[n];
+        int[] toLeft = new int[n];
+        int[] toRight = new int[n];
         
-        
-        // COPY PASTED DO AGAIN TOMORROW
-        
-        
-        int N = dominoes.length();
-        int[] indexes = new int[N+2];
-        char[] symbols = new char[N+2];
-        int len = 1;
-        indexes[0] = -1;
-        symbols[0] = 'L';
-
-        for (int i = 0; i < N; ++i)
-            if (dominoes.charAt(i) != '.') {
-                indexes[len] = i;
-                symbols[len++] = dominoes.charAt(i);
-            }
-
-        indexes[len] = N;
-        symbols[len++] = 'R';
-
-        char[] ans = dominoes.toCharArray();
-        for (int index = 0; index < len - 1; ++index) {
-            int i = indexes[index], j = indexes[index+1];
-            char x = symbols[index], y = symbols[index+1];
-            char write;
-            if (x == y) {
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = x;
-            } else if (x > y) { // RL
-                for (int k = i+1; k < j; ++k)
-                    ans[k] = k-i == j-k ? '.' : k-i < j-k ? 'R' : 'L';
-            }
+        int nearestRight = -1;
+        for(int i = 0; i < n; i++) {
+            char c = dominoes.charAt(i);
+            if(c == 'L') nearestRight = -1;
+            else if(c == 'R') nearestRight = i;
+            toRight[i] = nearestRight;
         }
 
-        return String.valueOf(ans);
+        int nearestLeft = -1;
+        for(int j = n - 1; j >= 0; j--) {
+            char c = dominoes.charAt(j);
+            if(c == 'R') nearestLeft = -1;
+            else if(c == 'L') nearestLeft = j;
+            toLeft[j] = nearestLeft;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            char c = dominoes.charAt(i);
+            int left = toLeft[i] == -1? Integer.MAX_VALUE : Math.abs(i - toLeft[i]);
+            int right = toRight[i] == -1? Integer.MAX_VALUE : Math.abs(i - toRight[i]);
+            
+            if(c == 'L' || c == 'R') ans[i] = c;
+            else if(left > right) ans[i] = 'R';
+            else if(left < right) ans[i] = 'L';
+            else ans[i] = '.';
+        }
+        
+        return new String(ans);
     }
 }
