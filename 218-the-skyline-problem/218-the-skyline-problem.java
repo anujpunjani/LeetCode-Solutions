77dbe6c3e1dc65787c1228564716495891e56bb6
Anuj Punjani <some4gun@gmail.com>
2022-10-01 12:16:10 +0530

    Time: 256 ms (36.58%), Space: 45.3 MB (94.69%) - LeetHub

--- 218-the-skyline-problem/218-the-skyline-problem.java
+++ 218-the-skyline-problem/218-the-skyline-problem.java
@@ -1,43 +1,33 @@
 class Solution {
-    // TC: O(nlogn)
     public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> ans = new ArrayList();
+        List<int[]> heights = new ArrayList();
         
-        
-        // COPY PASTED DO AGAIN
-        
-        
-        List<List<Integer>> res = new ArrayList<>();
-        List<int[]> heights = new ArrayList<>();
-
-        transformBuilding(buildings, heights); // O(n)
-        Collections.sort(heights, (a,b) -> (a[0]==b[0]) ? a[1] -b[1] : a[0] -b[0]); // O(nlogn)
-
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b) -> (b-a));
-        pq.offer(0);
-        int preMax = 0;
-        for(int[] height : heights){
-            if(height[1]<0){
-                pq.offer(-height[1]);
-            } else{
-                pq.remove(height[1]); //O(logn)
-            }
-            int currMax = pq.peek();
-            if(currMax != preMax){
-                List<Integer> subRes = new ArrayList<>();
-                subRes.add(height[0]);
-                subRes.add(currMax);
-                res.add(subRes);
-                preMax = currMax;
-            }
-        }
-        return res;
-    }
-
-    private void transformBuilding(int[][]buildings, List<int[]>heights) {
-
-        for(int[] building : buildings){
+        for(int[] building : buildings) {
             heights.add(new int[]{building[0], -building[2]});
             heights.add(new int[]{building[1], building[2]});
         }
+        
+        Collections.sort(heights, (a, b) -> a[0] == b[0]? a[1] - b[1] : a[0] - b[0]);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue(Collections.reverseOrder());
+        pq.add(0);
+        
+        int Max = 0;
+        for(int[] height : heights) {
+            if(height[1] < 0)
+                pq.add(-height[1]);
+            else
+                pq.remove(height[1]);
+            
+            int currentMax = pq.peek();
+            if(currentMax != Max) {
+                ans.add(Arrays.asList(height[0], currentMax));
+                Max = currentMax;
+            }
+            
+        }
+        
+        return ans;
     }
 }
