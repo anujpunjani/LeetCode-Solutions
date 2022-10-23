05ca8bdef5da60bba64b734dc767a6f7da583035
Anuj Punjani <some4gun@gmail.com>
2022-10-23 12:23:51 +0530

    Time: 41 ms (76.64%), Space: 55.4 MB (81.31%) - LeetHub

--- 1410-html-entity-parser/1410-html-entity-parser.java
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -1,46 +1,11 @@
 class Solution {
-    private void fillMap(HashMap<String, String> map) {
-        map.put("&quot;", "\"");
-        map.put("&apos;", "'");
-        map.put("&amp;", "&");
-        map.put("&gt;", ">");
-        map.put("&lt;", "<");
-        map.put("&frasl;", "/");
-    }
     public String entityParser(String text) {
-        StringBuilder sb = new StringBuilder("");
-        StringBuilder symbol = new StringBuilder("");
-        boolean and = false;
-        
-        HashMap<String, String> map = new HashMap();
-        fillMap(map);
-        
-        for(int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            
-            if(c == '&') {
-                if(and) {
-                    sb.append(symbol);
-                    symbol = new StringBuilder("");
-                } else
-                    and = true;
-            }
-            
-            if(c == ';') {
-                symbol.append(c);
-                String temp = symbol.toString();
-                if(map.containsKey(temp)) sb.append(map.get(temp));
-                else sb.append(symbol);
-                symbol = new StringBuilder("");
-                and = false;
-            }
-            else if(and) 
-                symbol.append(c);
-            else
-                sb.append(c);
-                
-        }
-        
-        return sb.append(symbol).toString();
+        text = text.replace("&quot;", "\"");
+        text = text.replace("&apos;", "'");
+        text = text.replace("&gt;", ">");
+        text = text.replace("&lt;", "<");
+        text = text.replace("&frasl;", "/");
+        text = text.replace("&amp;", "&");
+        return text;
     }
 }
