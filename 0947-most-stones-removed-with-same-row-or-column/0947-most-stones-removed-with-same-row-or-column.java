587f17973f8c8d367c9b2021da27949cb3d075ba
Anuj Punjani <some4gun@gmail.com>
2022-11-15 10:33:32 +0530

    Time: 81 ms (23.01%), Space: 48.9 MB (70.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,29 +1,27 @@
 class Solution {
- 
     
-    // COPY PASTED DO IT AGAIN
+    private void dfs(int idx, int[][] stones, boolean[] vis) {
+        
+        if(vis[idx]) return;
+        
+        vis[idx] = true;
+        
+        for(int i = 0; i < stones.length; i++)
+            if(stones[idx][0] == stones[i][0] || stones[idx][1] == stones[i][1])
+                dfs(i, stones, vis);
+    }
     
     public int removeStones(int[][] stones) {
- 
-        HashSet<Pair <Integer,Integer>> visited = new HashSet<>(); 
-        int ans = stones.length;
-        for(int i=0;i<stones.length;i++){
-            if(!visited.contains(new Pair <Integer,Integer> (stones[i][0],stones[i][1]))){
-                dfs(stones,visited,stones[i][0],stones[i][1]);
-                ans--;
-            }
+        boolean[] vis = new boolean[stones.length];
+        
+        int remaining = 0;
+        for(int i = 0; i < stones.length; i++){
+            if(vis[i]) continue;
+            remaining++;
+            dfs(i, stones, vis);
         }
-        return ans;
+        
+        
+        return stones.length - remaining;
     }
-    
-    void dfs(int[][] stones,HashSet<Pair <Integer,Integer>> visited,int i,int j){
-        visited.add(new Pair<Integer,Integer>(i,j));
-        for(int k=0;k<stones.length;k++){
-            if(!visited.contains(new Pair <Integer,Integer> (stones[k][0],stones[k][1]))){
-                if(i==stones[k][0] || j==stones[k][1])
-                    dfs(stones,visited,stones[k][0],stones[k][1]);
-            }
-        }
-    }
-    
 }
