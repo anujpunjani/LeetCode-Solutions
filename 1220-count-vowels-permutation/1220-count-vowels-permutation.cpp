fdc6a0be2ad6293383da4d6b5adca7a9a5cba498
Anuj Punjani <some4gun@gmail.com>
2022-12-09 19:29:42 +0530

    Time: 5 ms (81.20%), Space: 5.9 MB (93.16%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,19 +4,19 @@ public:
         int mod = (int)1e9 + 7;
         long a = 1, e = 1, i = 1, o = 1, u = 1;
         
-        for(int j = 2; j <= n; j++) {
+        for(int j = 1; j < n; j++) {
             
-            long new_a = (e + i + u) % mod;
-            long new_e = (a + i) % mod;
-            long new_i = (e + o) % mod;
-            long new_o = i % mod;
-            long new_u = (i + o) % mod;
+            int temp_a = (e + u + i) % mod;
+            int temp_e = (a + i) % mod;
+            int temp_i = (e + o) % mod;
+            int temp_o = i % mod;
+            int temp_u = (o + i) % mod;
             
-            a = new_a;
-            e = new_e;
-            i = new_i;
-            o = new_o;
-            u = new_u;
+            a = temp_a;
+            e = temp_e;
+            i = temp_i;
+            o = temp_o;
+            u = temp_u;
         }
         
         return (a + e + i + o + u) % mod;
