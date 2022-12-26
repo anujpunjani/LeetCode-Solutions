47005a37665386d1c28f1c402d99c9a26aaf370c
Anuj Punjani <some4gun@gmail.com>
2022-12-26 12:03:01 +0530

    Time: 55 ms (98.99%), Space: 48.5 MB (48.80%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     
-    bool canJump(int src, int dest, vector<int> &nums, vector<int> &dp) {
-        if(src == dest) return true;
-        if(src > dest) return false;
-        if(dp[src] != -1) return 1 == dp[src];
+//     bool canJump(int src, int dest, vector<int> &nums, vector<int> &dp) {
+//         if(src == dest) return true;
+//         if(src > dest) return false;
+//         if(dp[src] != -1) return 1 == dp[src];
         
-        bool temp = false;
+//         bool temp = false;
         
-        for(int i = 1; i <= nums[src]; i++) {
-            temp = temp || canJump(src + i, dest, nums, dp);
-            if(temp) break;
-        }
+//         for(int i = 1; i <= nums[src]; i++) {
+//             temp = temp || canJump(src + i, dest, nums, dp);
+//             if(temp) break;
+//         }
         
-        dp[src] = temp? 1 : 0;
-        return temp;
-    }
+//         dp[src] = temp? 1 : 0;
+//         return temp;
+//     }
     
-    bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return canJump(0, nums.size() - 1, nums, dp);
+//     bool canJump(vector<int>& nums) {
+//         vector<int> dp(nums.size(), -1);
+//         return canJump(0, nums.size() - 1, nums, dp);
+//     }
+    
+    bool canJump(vector<int> &nums) {
+        
+         int boundary = 0;
+         for(int i = 0; i <= boundary; i++) {
+             
+             boundary = max(boundary,i + nums[i]);
+          
+             if(boundary >= nums.size()-1)
+                return true;
+         } 
+         
+        return false;
     }
 };
