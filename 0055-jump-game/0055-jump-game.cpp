9e77c7cf2e5e8b14eca707974ef3321be9b1ca1a
Anuj Punjani <some4gun@gmail.com>
2022-12-26 11:59:54 +0530

    Time: 1542 ms (10.74%), Space: 51.1 MB (10.95%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    bool canJump(int src, int dest, vector<int> &nums, vector<int> &dp) {
+        if(src == dest) return true;
+        if(src > dest) return false;
+        if(dp[src] != -1) return 1 == dp[src];
+        
+        bool temp = false;
+        
+        for(int i = 1; i <= nums[src]; i++) {
+            temp = temp || canJump(src + i, dest, nums, dp);
+            if(temp) break;
+        }
+        
+        dp[src] = temp? 1 : 0;
+        return temp;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return canJump(0, nums.size() - 1, nums, dp);
+    }
+};
