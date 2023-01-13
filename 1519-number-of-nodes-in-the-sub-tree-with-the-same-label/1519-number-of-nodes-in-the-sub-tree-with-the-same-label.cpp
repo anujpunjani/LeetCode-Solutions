bcb05054c357a9f6409119cec9351d1e1c101554
Anuj Punjani <some4gun@gmail.com>
2023-01-13 12:49:45 +0530

    Time: 686 ms (72.86%), Space: 234.6 MB (28.57%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     
-    vector<int> dfs(int src, int parent, vector<vector<int>> &graph, unordered_map<int, char> &nodes, vector<int> &ans) {
+    vector<int> dfs(int src, int parent, vector<vector<int>> &graph, string &labels, vector<int> &ans) {
         
         vector<int> freq(26, 0);
-        freq[nodes[src]-'a']++;
+        freq[labels[src]-'a']++;
         
         for(int nbr : graph[src]) {
             if(nbr == parent) continue;
-            vector<int> temp = dfs(nbr, src, graph, nodes, ans);
+            vector<int> temp = dfs(nbr, src, graph, labels, ans);
             for(int i = 0; i < temp.size(); i++)
                 freq[i] += temp[i];
         }
         
-        ans[src] = freq[nodes[src]-'a'];
+        ans[src] = freq[labels[src]-'a'];
         return freq;
     }
     
     vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
         vector<vector<int>> graph(n);
-        unordered_map<int, char> nodes;
-        
-        for(int i = 0; i < labels.size(); i++)
-            nodes[i] = labels[i];
         
         for(auto edge : edges) {
             graph[edge[0]].push_back(edge[1]);
@@ -30,7 +26,7 @@ public:
         }
         
         vector<int> ans(n, 0);
-        dfs(0, -1, graph, nodes, ans);
+        dfs(0, -1, graph, labels, ans);
         
         return ans;
     }
